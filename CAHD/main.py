1ed5185301cf828c30c0a840c3be8b1eb743e5cb
Matte <matteo.salv96@gmail.com>
2021-03-20 18:19:03 +0100

    modificato README.md, creato metodo main con i parametri di input e creata la classe DataFrame.py, destinata alla creazione del dataframe su cui applicare poi l'algoritmo RCM.

--- CAHD/main.py
+++ CAHD/main.py
@@ -1,6 +1,25 @@
-import pandas
-import numpy
-import scipy
+import pandas as pd
+import DataFrame
+
+if __name__ == "__main__":
+    print("dimensioni del dataset:")
+    size = input()
+    size = 1000
+    print("Grado di Privacy:")
+    p = input()
+    #p = 10
+    print("numero di item sensibili:")
+    s = input()
+    #s = 10;
+    print("numero di Quasi-Identifier:")
+    qi = input()
+    #qi = 4;
+    print("valore di alpha desiderato (ottimale = 3):")
+    alpha = input()
+    #alpha = 3;
+
+    nameFile = "BMS1_spmf.txt"
+    df = DataFrame(nameFile, size, s, qi)
 
 
-print("merda")
+
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Data-Protection-&-Privacy
-Repository per il progetto di DPP del prof. Alessio Merlo
+# Progetto per il corso di Data Protection & Privacy
+Il progetto si pone come obiettivo l'anonimizzazione di **dati transazionali**.
+
+il database di riferimento è reperibile liberamente a questo [link](http://www.philippe-fournier-viger.com/spmf/index.php?link=datasets.php).
+Nello specifico, è stato utilizzato il dataset denominato [BMSWebView1 (Gazelle) ( KDD CUP 2000)](http://www.philippe-fournier-viger.com/spmf/datasets/BMS1_spmf)
