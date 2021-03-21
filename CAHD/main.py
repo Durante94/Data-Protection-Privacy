d8e826f7790f91ddbf2a49df6ec022005f962ab4
Matte <matteo.salv96@gmail.com>
2021-03-21 11:39:18 +0100

    modifiche al main

--- CAHD/main.py
+++ CAHD/main.py
@@ -1,25 +1,26 @@
 import pandas as pd
-import DataFrame
+from DataFrame import DataFrame
+
 
 if __name__ == "__main__":
     print("dimensioni del dataset:")
     size = input()
-    size = 1000
+    # size = 1000
     print("Grado di Privacy:")
     p = input()
-    #p = 10
+    # p = 10
     print("numero di item sensibili:")
     s = input()
-    #s = 10;
+    # s = 10;
     print("numero di Quasi-Identifier:")
     qi = input()
-    #qi = 4;
+    # qi = 4;
     print("valore di alpha desiderato (ottimale = 3):")
     alpha = input()
-    #alpha = 3;
+    # alpha = 3;
 
     nameFile = "BMS1_spmf.txt"
-    df = dataframe(nameFile, size, s, qi)
+    df = DataFrame(nameFile, size, s, qi)
 
 
 
