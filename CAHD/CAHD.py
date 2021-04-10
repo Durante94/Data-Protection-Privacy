dbff4e22529b598f776ef6b3fca095b458e429a4
Matte <matteo.salv96@gmail.com>
2021-04-10 15:22:06 +0200

    istogramma creato

--- CAHD/CAHD.py
+++ CAHD/CAHD.py
@@ -4,27 +4,31 @@ import numpy as np
 
 class CAHD:
 
-    def __init__(self, df, p, alpha, SDCols, QICols):
+    def __init__(self, df, p, alpha, SDvals, QIvals):
         self.df = df
         self.p = p
         self.alpha = alpha
-        self.SDCols = SDCols
-        self.QICols = QICols
-
+        self.SDvals = SDvals
+        self.QIvals = QIvals
+        self.hist = dict()
+        self.prova = dict()
 
     def compute_histogram(self):
-        """
-        histogram = dict()
-        for sd in self.SDCols:
-            histogram[str(sd)] = self.df[str(sd)]
-        return histogram
-        """
-        self.hist = dict(self.df[self.SDCols].sum())
+        # DEBUG (da rimuovere)
+        for i in self.SDvals:
+            self.hist[i] = 0
+        for index, row in self.df.iterrows():
+            for column in self.df:
+                if column in self.SDvals and (self.df.loc[row.name, column] == True):
+                    self.hist[column] += 1
         print(self.hist)
+
+        self.prova = dict(self.df[self.SDvals].sum())
+        print(self.prova)
 
     def checkPrivacy(self):
         for value in self.hist.values():
-            if value * self.p >= len(self.df)-1:
+            if value * self.p >= len(self.df) - 1:
                 return False
         return True
 
@@ -37,10 +41,6 @@ class CAHD:
         while not satisfiable and self.p > 0:
             satisfiable = self.checkPrivacy()
             if not satisfiable:
-                self.checkPrivacy()-1
+                self.checkPrivacy() - 1
         print(("Privacy degree satisfiable: ", self.p))
         remaining = len(self.df)
-
-
-
-
--- CAHD/main.py
+++ CAHD/main.py
@@ -5,7 +5,7 @@ from Plot import plot
 if __name__ == "__main__":
     # print("numero di item:")
     # size = input()
-    size = 14
+    size = 450
     # print("Grado di Privacy:")
     # p = input()
     p = 10
